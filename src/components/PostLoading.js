af4ae3a0a517884daefdc21b32885fedc71aed77
paul <paulbrowne2@gmail.com>
2022-02-16 13:17:35 +0000

    Added if statement in PostLoading component to return component props

--- src/components/PostLoading.js
+++ src/components/PostLoading.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function PostLoading(Component) {
     return function PostLoadingComponent({ isLoading, ...props}) {
-        
+        if (!isLoading) return <Component {...props} />
         return (
             <p style={{ fontSize: '25px' }}>
                 Waiting for data to load!...
