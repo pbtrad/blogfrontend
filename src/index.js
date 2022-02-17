0816512e208e837f623addbb33f9d4fefd5ba1a2
paul <paulbrowne2@gmail.com>
2022-02-17 17:14:23 +0000

    Created login and logout pages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from  './App';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Register from './components/register';
+import Login from './components/login';
+import Logout from './components/logout';
 
 const routing = (
   <Router>
@@ -14,6 +16,8 @@ const routing = (
       <Routes>
         <Route exact path="/" element={<App />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
       </Routes>
       <Footer />
     </React.StrictMode>
